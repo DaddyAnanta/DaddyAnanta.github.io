45b6beceab2fdba91b5632e70a75486c5f6a9e7e
Daddy Ananta <bapakkupresiden@protonmail.com>
2025-04-30 11:19:03 +0700

    Update loading-screen.js

--- static/js/loading-screen.js
+++ static/js/loading-screen.js
@@ -1,10 +1,17 @@
-window.addEventListener('load', function () {
-    const loadingScreen = document.getElementById('loading-screen');
-    if (loadingScreen) {
-      loadingScreen.style.opacity = '0';
-      loadingScreen.style.transition = 'opacity 0.5s ease';
-      setTimeout(() => {
-        loadingScreen.style.display = 'none';
-      }, 500);
+
+  let percent = 0;
+  const interval = setInterval(() => {
+    if (percent < 99) {
+      percent += Math.floor(Math.random() * 3) + 1; // naik acak 1-3%
+      if (percent > 99) percent = 99;
+      document.getElementById("progress-percent").textContent = percent + "%";
     }
+  }, 60);
+
+  window.addEventListener('load', () => {
+    clearInterval(interval);
+    document.getElementById("progress-percent").textContent = "100%";
+    setTimeout(() => {
+      document.getElementById("loading-screen").style.display = "none";
+    }, 500); // jeda sedikit sebelum menghilangkan loader
   });
